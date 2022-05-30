02adb5fa9975e4a0923b05adde49f838d34bd2d3
fiandinw <alifiandinursanni@gmail.com>
2022-05-30 14:02:42 +0700

    faq redesign

--- components/FaqAccordion.js
+++ components/FaqAccordion.js
@@ -9,10 +9,10 @@ function FaqAccordion(props) {
 
   return (
     <div className="flex flex-col w-full items-center px-16 2xl:px-32">
-      <div onClick={toggleActive} className={`text-sigap-ijo ${!isActive ? 'rounded-xl' : 'rounded-t-xl'} p-4 border-2 text-xl flex items-center justify-between w-full cursor-pointer`}>
+      <div onClick={toggleActive} className={`text-sigap-ijo font-bold ${!isActive ? 'rounded-xl' : 'rounded-t-xl'} p-4 border-2 text-xl flex items-center justify-between w-full cursor-pointer`}>
         <p>{props.title}</p><i className={`fa ${!isActive ? "fa-plus" : "fa-minus"} fa-xl`} aria-hidden="true"></i>
       </div>
-      <div className={`text-sigap-ijo text-lg rounded-b-lg w-full overflow-hidden ${!isActive ? 'h-0 px-0' : 'h-fit px-8 py-2 border-2 border-t-0'} transition-all`}>
+      <div className={`text-hitam text-lg rounded-b-lg w-full overflow-hidden ${!isActive ? 'h-0 px-0' : 'h-fit px-8 py-2 border-2 border-t-0'} transition-all`}>
         {props.description}
       </div>
     </div>
